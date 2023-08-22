66fc640701e61673b9c9bed16de26cef2dc3de8a
Cosimo-Colucci <ccoluccilavoro@gmail.com>
2023-08-22 17:39:58 +0200

    created DashboardController for Admin

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\DashboardController;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +22,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::prefix('admin')->name('admin.')->middleware('auth')->group(function () {
+    Route::get('/home', [ DashboardController::class, 'home'])->name('home');
+
+});
