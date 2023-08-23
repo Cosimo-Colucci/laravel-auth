0b1d26fc05ff12108f8f1b3c51e27338ebe7a08a
Cosimo-Colucci <ccoluccilavoro@gmail.com>
2023-08-23 17:22:58 +0200

    created route test for view projects

---
+++ resources/views/admin/projects/index.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-12">
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th scope="col">Id</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Progect Start</th>
+                    <th scope="col">Slug</th>
+                    <th scope="col">Owner</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th>1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                    <td>edit</td>
+                </tr>
+                <tr>
+                    <th>1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                    <td>edit</td>
+                </tr>
+                <tr>
+                    <th>1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                    <td>edit</td>
+                </tr>
+            </tbody>
+        </table>
+        </div>
+    </div>
+</div>
+@endsection
