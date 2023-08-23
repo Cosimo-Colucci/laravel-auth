68ec8be48d069c93fdf7b3ceefeb080622ad6e64
Cosimo-Colucci <ccoluccilavoro@gmail.com>
2023-08-23 18:01:49 +0200

    update proviter file for bootstrap

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -4,41 +4,42 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-12">
-        <table class="table table-hover">
-            <thead>
-                <tr>
-                    <th scope="col">Id</th>
-                    <th scope="col">Title</th>
-                    <th scope="col">Progect Start</th>
-                    <th scope="col">Slug</th>
-                    <th scope="col">Owner</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($projects as $project)
+            <table class="table table-hover">
+                <thead>
                     <tr>
-                        <th>
-                            {{$project->id}} 
-                        </th>
-                        <td>
-                            {{$project->title}} 
-                        </td>
-                        <td>
-                            {{$project->project_start}}    
-                        </td>
-                        <td>
-                            {{$project->slug}}
-                        </td>
-                        <td>
-                            {{$project->owner}}
-                        </td>
-                        <td>
-                            <a href="" class="btn btn-sm button-primary">View</a>
-                        </td>
+                        <th scope="col">Id</th>
+                        <th scope="col">Title</th>
+                        <th scope="col">Progect Start</th>
+                        <th scope="col">Slug</th>
+                        <th scope="col">Owner</th>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    @foreach ($projects as $project)
+                        <tr>
+                            <th>
+                                {{$project->id}} 
+                            </th>
+                            <td>
+                                {{$project->title}} 
+                            </td>
+                            <td>
+                                {{$project->project_start}}    
+                            </td>
+                            <td>
+                                {{$project->slug}}
+                            </td>
+                            <td>
+                                {{$project->owner}}
+                            </td>
+                            <td>
+                                <a href="" class="btn btn-sm button-primary">View</a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+            {{$projects->links()}}
         </div>
     </div>
 </div>
